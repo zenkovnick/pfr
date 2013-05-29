a33d6736acc084c86b2ede124a450c4cf9e3f789
Mykola Zenkov <mykola.zenkov@masterofcode.com>
2013-05-29 13:32:02 +0300

    Slicing header

--- apps/frontend/modules/menu/templates/_header_menu.php
+++ apps/frontend/modules/menu/templates/_header_menu.php
@@ -2,9 +2,9 @@
 <div class="header-account-information">
     <span class="header-account-avatar">
         <?php if(isset($account_id) && isset($account) && $account->getPhoto()): ?>
-            <?php echo image_tag(getThumbnail('avatar/'.$account->getPhoto(), 40), array('alt' => '')) ?>
+            <?php echo image_tag(getThumbnail('avatar/'.$account->getPhoto(), 60), array('alt' => '')) ?>
         <?php else: ?>
-            <?php echo image_tag(getThumbnail('../images/no_logo.jpg', 40), array('alt' => '')) ?>
+            <?php echo image_tag(getThumbnail('../images/no_logo.jpg', 60), array('alt' => '')) ?>
         <?php endif ?>
     </span>
     <span class="header-account-title">
@@ -16,27 +16,33 @@
     </span>
 </div>
 <div class="header-link-block">
-    <span class="header-dashboard">
+    <span class="header-dashboard header-link">
         <a href="<?php echo isset($account_id) ? url_for("@dashboard?account_id={$account_id}") : "#" ?>">Dashboard</a>
     </span>
-    <span class="header-settings">
+    <span class="header-settings header-link">
         <a href="<?php echo isset($account_id) ? url_for("@settings?account_id={$account_id}") : "#" ?>">Settings</a>
     </span>
 </div>
-<div class="header-user-information">
-    <span class="header-user-name">
-        Keep the blue side up, <?php echo $sf_user->getGuardUser()->getFirstName(); ?>
-    </span>
-    <span class="header-user-email">
-        <?php echo $sf_user->getGuardUser()->getUsername(); ?>
-    </span>
-    <span class="header-user-email">
-        <?php if($sf_user->getGuardUser()->getPhoto()): ?>
-            <?php echo image_tag(getThumbnail('avatar/'.$sf_user->getGuardUser()->getPhoto(), 40), array('alt' => '')) ?>
-        <?php else: ?>
-            <?php echo image_tag(getThumbnail('../images/no_logo.jpg', 40), array('alt' => '')) ?>
-        <?php endif ?>
-    </span>
+<div class="header-user-information-wrapper">
+    <div class="header-user-information">
+        <span class="header-user-avatar">
+            <?php if($sf_user->getGuardUser()->getPhoto()): ?>
+                <?php echo image_tag(getThumbnail('avatar/'.$sf_user->getGuardUser()->getPhoto(), 40), array('alt' => '')) ?>
+            <?php else: ?>
+                <?php echo image_tag(getThumbnail('../images/no_logo.jpg', 40), array('alt' => '')) ?>
+            <?php endif ?>
+        </span>
+        <div class="user-wrapper">
 
+            <span class="header-user-name">
+                Keep the blue side up, <?php echo $sf_user->getGuardUser()->getFirstName(); ?>
+            </span>
+        <br />
+            <span class="header-user-email">
+                <?php echo $sf_user->getGuardUser()->getUsername(); ?> (<a href="<?php echo url_for('@signout') ?>">Sign Out</a>)
+            </span>
+        </div>
+
+    </div>
 </div>
 
