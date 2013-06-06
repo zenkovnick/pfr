1d9510c3da72e7c57eff344a481397b629a4580a
Irina Korolyuk <irina@irina-PC.(none)>
2013-06-06 17:17:53 +0300

    create account page

--- apps/frontend/modules/registration/templates/createAccountSuccess.php
+++ apps/frontend/modules/registration/templates/createAccountSuccess.php
@@ -5,16 +5,25 @@
 
 <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 <script type="text/javascript">
+    jQuery('body').addClass('create-account');
     jQuery(document).ready(function(){
         init_crop('<?php echo url_for('@create_account_upload_avatar'); ?>', '<?php echo url_for('@create_account_get_widget'); ?>',
             '/images/no_avatar.png', [32, 32], 'photo', 'account_photo', 'avatar_container', 'account_uploaded_photo');
     });
 </script>
 
+<?php slot('header') ?>
+    <?php include_partial('menu/header_logo')?>
+    <?php if($sf_user->isAuthenticated()): ?>
+        <a class="sign-out" href="<?php echo url_for("@signout"); ?>">Sign Out</a>
+    <?php endif ?>
+<?php end_slot() ?>
 
-<?php if($sf_user->isAuthenticated()): ?>
-    <a href="<?php echo url_for("@signout"); ?>">Sign Out</a>
-<?php endif ?>
+<h1>Create an Account</h1>
+<p>
+   Welcome! If you know your company is already using Preflight Risk, email your administrator to request they add you
+   as a user. Otherwise, create an account now.
+</p>
 
 <form id="create_account_form" method="post">
     <?php echo($form->renderHiddenFields()) ?>
@@ -22,10 +31,10 @@
 
 
     <ul>
-        <li class="input-block"><?php include_partial('registration/field', array('field' => $form['title'], 'placeholder' => 'Company, Organization or Name')) ?></li>
-        <li class="input-block"><?php include_partial('registration/field', array('field' => $form['chief_pilot_email'], 'placeholder' => 'Chief Pilot\'s Email (if any)')) ?></li>
+        <li class="input-block"><?php include_partial('registration/field', array('field' => $form['title'], 'placeholder' => 'Company, Organization or Name', 'label' => false)) ?></li>
+        <li class="input-block"><?php include_partial('registration/field', array('field' => $form['chief_pilot_email'], 'placeholder' => 'Chief Pilot\'s Email (if any)', 'label' => false)) ?></li>
         <li class="photo-block"><?php include_partial('registration/avatar_field', array('field' => $form['photo'])) ?></li>
-        <li><button type="submit">Create Account</button></li>
+        <li><button class="btn btn-blue" type="submit">Create Account</button></li>
     </ul>
 
 </form>
