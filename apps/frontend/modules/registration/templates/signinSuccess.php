2dc0262ffa235f540bf803debeb05066ed0461dc
Irina Korolyuk <irina@irina-PC.(none)>
2013-06-06 14:38:39 +0300

    placeholder behaviour changed

--- apps/frontend/modules/registration/templates/signinSuccess.php
+++ apps/frontend/modules/registration/templates/signinSuccess.php
@@ -28,17 +28,17 @@
 
 <script type="text/javascript">
     jQuery('body').addClass('sign-in');
-    jQuery('[placeholder]').focus(function() {
-        var input = jQuery(this);
-        if (input.val() == input.attr('placeholder')) {
-            input.val('');
-            input.removeClass('placeholder');
-        }
-    }).blur(function() {
-            var input = jQuery(this);
-            if (input.val() == '' || input.val() == input.attr('placeholder')) {
-                input.addClass('placeholder');
-                input.val(input.attr('placeholder'));
-            }
-        }).blur();
+//    jQuery('[placeholder]').focus(function() {
+//        var input = jQuery(this);
+//        if (input.val() == input.attr('placeholder')) {
+//            input.val('');
+//            input.removeClass('placeholder');
+//        }
+//    }).blur(function() {
+//            var input = jQuery(this);
+//            if (input.val() == '' || input.val() == input.attr('placeholder')) {
+//                input.addClass('placeholder');
+//                input.val(input.attr('placeholder'));
+//            }
+//        }).blur();
 </script>
