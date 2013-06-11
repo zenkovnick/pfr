62f9b202f1ca829e2715755331de0fc1e2f6c9f8
Irina Korolyuk <irina@irina-PC.(none)>
2013-06-11 18:00:25 +0300

    fix

--- apps/frontend/modules/flight/templates/_risk_field.php
+++ apps/frontend/modules/flight/templates/_risk_field.php
@@ -31,7 +31,7 @@ if(isset($help)){
         <span class="risk">
                 <?php echo $risk ?>
         </span>
-        <span class="note"><span></span>
+        <span class="note hidden">
                 <?php echo $note ?>
         </span>
     </div>
