54b4678052364e64591cd0639d15db0f9f5b35f6
Mykola Zenkov <mykola.zenkov@masterofcode.com>
2013-06-13 14:40:31 +0300

    First Time Slice

--- apps/frontend/modules/dashboard/templates/firstTimeSuccess.php
+++ apps/frontend/modules/dashboard/templates/firstTimeSuccess.php
@@ -1,42 +1,55 @@
 <?php slot('header') ?>
     <?php include_partial('menu/header_menu', array('account' => $account)); ?>
 <?php end_slot() ?>
-<a href="<?php echo url_for("@create_flight?account_id={$account->getId()}")?>">New Flight</a>
-<div>
-    <ul class="conditions">
-        <li class="signup-condition condition">
-            <span>Sign up for Preflight Risk</span>
-        </li>
-        <li class="plane-condition condition">
+<div class="first-time-dashboard">
+    <div class="first-time-header">
+        <h1>Dashboard</h1>
+        <div class="new-flight-wrapper">
             <?php if($account->getHasPlane()): ?>
-                <span>Add your first plane</span>
+                <a class="btn btn-blue new-flight" href="<?php echo url_for("@create_flight?account_id={$account->getId()}")?>">New Flight</a>
             <?php else: ?>
-                <a href="<?php echo url_for("@settings?account_id={$account->getId()}#planes") ?>">Add your first plane</a>
+                <a class="btn btn-grey new-flight disabled" href="">New Flight</a>
             <?php endif ?>
-        </li>
-        <li class="risk-condition condition">
-            <?php if($account->getHasModifiedForm()): ?>
-                <span>Edit your default risk assessment form</span>
-            <?php else: ?>
-                <a href="<?php echo url_for("@form?id={$form->getId()}") ?>">Edit your default risk assessment form</a>
-            <?php endif ?>
-        </li>
-        <li class="pilot-condition condition">
-            <?php if($account->getHasPilot() || $account->getHasSkippedPilot()): ?>
-                <span>Invite other pilots</span>
-            <?php else: ?>
-                <a href="<?php echo url_for("@settings?account_id={$account->getId()}#pilots") ?>">Invite other pilots</a>
-                <a class="skip-pilot" href="#">Skip</a>
-            <?php endif ?>
-        </li>
-        <li class="flight-condition condition">
-            <?php if($account->getHasFlight()): ?>
-                <span>Measure your first flight's risk</span>
-            <?php else: ?>
-                <a href="<?php echo url_for("@create_flight?account_id={$account->getId()}") ?>#">Measure your first flight's risk</a>
-            <?php endif ?>
-        </li>
-    </ul>
+        </div>
+    </div>
+
+    <div class="first-time-wrapper">
+        <h2 class="first-time-title">Get started!</h2>
+        <ul class="conditions">
+            <li class="signup-condition condition">
+                <span>1. Sign up for Preflight Risk</span>
+            </li>
+            <li class="plane-condition condition">
+                <?php if($account->getHasPlane()): ?>
+                    <span>2 .Add your first plane</span>
+                <?php else: ?>
+                    <a href="<?php echo url_for("@settings?account_id={$account->getId()}#planes") ?>">2. Add your first plane</a>
+                <?php endif ?>
+            </li>
+            <li class="risk-condition condition">
+                <?php if($account->getHasModifiedForm()): ?>
+                    <span>3. Edit your default risk assessment form</span>
+                <?php else: ?>
+                    <a href="<?php echo url_for("@form?id={$form->getId()}") ?>">3. Edit your default risk assessment form</a>
+                <?php endif ?>
+            </li>
+            <li class="pilot-condition condition">
+                <?php if($account->getHasPilot() || $account->getHasSkippedPilot()): ?>
+                    <span>4. Invite other pilots</span>
+                <?php else: ?>
+                    <a href="<?php echo url_for("@settings?account_id={$account->getId()}#pilots") ?>">4. Invite other pilots</a>
+                    <a class="skip-pilot hidden" href="#">Skip</a>
+                <?php endif ?>
+            </li>
+            <li class="flight-condition condition">
+                <?php if($account->getHasFlight()): ?>
+                    <span>5. Measure your first flight's risk</span>
+                <?php else: ?>
+                    <a href="<?php echo url_for("@create_flight?account_id={$account->getId()}") ?>#">5. Measure your first flight's risk</a>
+                <?php endif ?>
+            </li>
+        </ul>
+    </div>
 </div>
 <script type="text/javascript">
     function skipPilotCondition(event) {
@@ -49,14 +62,22 @@
             success: function(data){
                 if(data.result == "OK"){
                     jQuery("a", root_li).remove();
-                    root_li.html("<span>Invite other pilots</span>");
+                    root_li.html("<span>4. Invite other pilots</span>");
                 }
             }
 
         });
     }
 
+    function showSkip() {
+        jQuery("a.skip-pilot", this).removeClass("hidden");
+    }
+    function hideSkip() {
+        jQuery("a.skip-pilot", this).addClass("hidden");
+    }
     jQuery(document).ready(function(){
         jQuery("a.skip-pilot").bind('click', skipPilotCondition);
+        jQuery("a.disabled").click(function(event){event.preventDefault()});
+        jQuery("li.pilot-condition").bind("mouseover", showSkip).bind("mouseout", hideSkip);
     });
 </script>
