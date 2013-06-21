c19ddfe079045f3ab93fe859fad4f3da85eb0f36
daniel <daniel@daniel-laptop.(none)>
2013-06-21 09:53:52 +0300

    dashboard page is styled

--- apps/frontend/modules/dashboard/templates/_dashboard_content.php
+++ apps/frontend/modules/dashboard/templates/_dashboard_content.php
@@ -4,19 +4,20 @@
 <div class="additional-info">
     <ul>
         <li>
-            <span><?php echo number_format($additional_info['average_risk'], 1) ?></span>
-            <span>Average Risk</span>
+            <span class="number"><?php echo number_format($additional_info['average_risk'], 1) ?></span>
+            <span class="caption">Average Risk</span>
         </li>
         <li>
-            <span><?php echo $additional_info['flights_count'] ?></span>
-            <span>Flights</span>
+            <span class="number"><?php echo $additional_info['flights_count'] ?></span>
+            <span class="caption">Flights</span>
         </li>
         <li>
-            <span><?php echo number_format($additional_info['average_mitigation'], 1) ?></span>
-            <span>Average Mitigation</span>
+            <span class="number"><?php echo number_format($additional_info['average_mitigation'], 1) ?></span>
+            <span class="caption">Average Mitigation</span>
         </li>
     </ul>
 </div>
+
 <div class="flight-list-wrapper">
     <?php include_partial('dashboard/flight_list', array('pager' => $pager, 'account' => $account)) ?>
 </div>
---
+++ web/js/enable_selectmenu.js
@@ -0,0 +1,8 @@
+$(window).load(function() {
+    
+    $('#flight_filter_plane').selectmenu();
+    $('#flight_filter_pilot').selectmenu();
+    $('#flight_filter_date').selectmenu();
+    $('#flight_filter_sort').selectmenu();
+    
+});
