ec7b166215ce069cdbf9db32d2127ea1b7d1e6f0
Irina Korolyuk <irina@irina-PC.(none)>
2013-07-01 16:14:18 +0300

    orientationchange fix

--- apps/frontend/modules/builder/templates/indexSuccess.php
+++ apps/frontend/modules/builder/templates/indexSuccess.php
@@ -865,8 +865,13 @@
 
             jQuery('ul.form-fields').css('margin-left', -jQuery('ul.form-fields').width()/2-20+'px');
             if(isAndroid) {
-                jQuery('ul.form-fields').css('margin-left', -(jQuery(window).height()-40)/2-35+'px');
-                alert(window.orientation);
+                if (window.orientation == 0) {
+                    jQuery('ul.form-fields').css('margin-left', -(jQuery(window).height()-40)/2-35+'px');
+                }
+                if (window.orientation == 90 || window.orientation == -90) {
+                    jQuery('ul.form-fields').css('margin-left', -(jQuery(window).height()-40)/2-30+'px');
+                }
+//                alert(window.orientation);
 
 
 
