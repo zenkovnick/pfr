d4bc4667199e6f15ca62b14eea2e97f7fdbb5595
Irina Korolyuk <irina@irina-PC.(none)>
2013-07-01 16:05:59 +0300

    orientationchange fix

--- apps/frontend/modules/builder/templates/indexSuccess.php
+++ apps/frontend/modules/builder/templates/indexSuccess.php
@@ -866,6 +866,7 @@
             jQuery('ul.form-fields').css('margin-left', -jQuery('ul.form-fields').width()/2-20+'px');
             if(isAndroid) {
                 jQuery('ul.form-fields').css('margin-left', -(jQuery(window).height()-40)/2-35+'px');
+                alert(jQuery(window).orientation);
 
 
 
