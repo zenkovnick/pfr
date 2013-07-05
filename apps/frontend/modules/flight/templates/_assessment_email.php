c398b9715a6dbc3bb16479e8a96beb45a6015724
Mykola Zenkov <mykola.zenkov@masterofcode.com>
2013-07-05 14:33:50 +0300

    Email; changes

--- apps/frontend/modules/flight/templates/_assessment_email.php
+++ apps/frontend/modules/flight/templates/_assessment_email.php
@@ -1,9 +1,9 @@
-<div class="assessment-wrapper" xmlns="http://www.w3.org/1999/html">
+<div class="assessment-wrapper">
     <div class="risk-assessment">
-        <h1 style="color: #000; text-align: center;"><?php echo $mitigation_info['message'] ?></h1>
-        <p style="color: #999; display: block; text-align: center;">
-            Flight <span style="font-weight: bold;"> #<?php echo $flight->getTripNumber() ?></span> from <span style="font-weight: bold;"><?php echo $flight->getAirportFrom() ?></span> to <span style="font-weight: bold;"><?php echo $flight->getAirportTo() ?></span>
-            in <span style="font-weight: bold;"><?php echo $flight->getPlane()->getTailNumber() ?></span> has a <?php echo $mitigation_info['type'] ?> risk factor with <span style="font-weight: bold;"><?php echo $flight->getRiskFactorSum() ?></span> out of <span style="font-weight: bold;">50</span>
+        <h1><?php echo $mitigation_info['message'] ?></h1>
+        <p>
+            Flight #<?php echo $flight->getTripNumber() ?> from <?php echo $flight->getAirportFrom()->getICAO() ?> to <?php echo $flight->getAirportTo()->getICAO() ?>
+            in <?php echo $flight->getPlane()->getTailNumber() ?> has a <?php echo $mitigation_info['type'] ?> risk factor with <?php echo $flight->getRiskFactorSum() ?> of 50
         </p>
 
         <span class="risk"><?php echo $flight->getRiskFactorSum() ?></span>
