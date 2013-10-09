15a8d4abd9bee9e25b61bb5e09d4c0ec437ffe28
Mykola Zenkov <mykola.zenkov@masterofcode.com>
2013-10-09 16:46:16 +0300

    Fix dashboard

--- apps/frontend/modules/dashboard/templates/_dashboard_content.php
+++ apps/frontend/modules/dashboard/templates/_dashboard_content.php
@@ -2,23 +2,23 @@
     <div id="chart" class="chart-block">
 
     </div>
+    <div class="additional-info">
+        <ul>
+            <li>
+                <span class="number"><?php echo number_format($additional_info['average_risk'], 1) ?></span>
+                <span class="caption">Average Risk</span>
+            </li>
+            <li>
+                <span class="number"><?php echo $additional_info['flights_count'] ?></span>
+                <span class="caption">Flights</span>
+            </li>
+            <li>
+                <span class="number"><?php echo number_format($additional_info['average_mitigation'], 1) ?></span>
+                <span class="caption">Average Mitigation</span>
+            </li>
+        </ul>
+    </div>
 <?php endif ?>
-<div class="additional-info">
-    <ul>
-        <li>
-            <span class="number"><?php echo number_format($additional_info['average_risk'], 1) ?></span>
-            <span class="caption">Average Risk</span>
-        </li>
-        <li>
-            <span class="number"><?php echo $additional_info['flights_count'] ?></span>
-            <span class="caption">Flights</span>
-        </li>
-        <li>
-            <span class="number"><?php echo number_format($additional_info['average_mitigation'], 1) ?></span>
-            <span class="caption">Average Mitigation</span>
-        </li>
-    </ul>
-</div>
 
 <div class="flight-list-wrapper">
     <?php include_partial('dashboard/flight_list', array('pager' => $pager, 'account' => $account)) ?>
