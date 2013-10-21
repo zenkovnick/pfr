d88c5c10d664587109435b82960a5ea5294281b5
Lebed Timofey <lebed.tima@gmail.com>
2013-10-21 12:57:53 +0300

    delete the text than is in the 'settings' are when adding an aircraft

--- apps/frontend/modules/settings/templates/_editPlane.php
+++ apps/frontend/modules/settings/templates/_editPlane.php
@@ -8,9 +8,6 @@
             <li class="input-block">
                 <?php include_partial("settings/field", array('field' => $form['tail_number'], 'class' => 'tail-number', 'placeholder' => 'Tail Number')); ?>
             </li>
-            <li class="hint-block">
-                <p>Adding this plane will give you the ability to measure its flights' risk over time, but be aware that by adding this plane, you will be adding <span>$5 every month</span> to your bill.</p>
-            </li>
             <li class="buttons-block">
                 <a href="" class="delete-plane remove-link">Delete</a>
                 <button class="btn btn-green" type="submit">Save</button>
