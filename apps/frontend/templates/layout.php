df6376d0f05b3a797c5599353a9373a9b75e09c5
Lebed Tima <lebed.tima@gmail.com>
2013-10-24 17:52:05 +0300

    modified doctype

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,7 +1,7 @@
 <?php use_helper('Thumbnail'); ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" xmlns:fb="http://ogp.me/ns/fb#">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>
