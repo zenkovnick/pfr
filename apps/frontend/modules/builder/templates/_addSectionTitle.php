42fd384ad17d339b8791fb2d2f9dccc431ee3b7c
Lebed Tima <lebed.tima@gmail.com>
2013-10-24 11:09:00 +0300

    added section title can delete

--- apps/frontend/modules/builder/templates/_addSectionTitle.php
+++ apps/frontend/modules/builder/templates/_addSectionTitle.php
@@ -2,6 +2,7 @@
     <input type="hidden" value="<?php echo $risk_factor->getId() ?>" class="question_id" />
     <div class="entry-header" style="background: #FFF !important;">
         <span class="question truncate"><?php echo $risk_factor->getQuestion() ?></span>
+        <a href="" class="delete-section" style="display: none;">Delete</a>
     </div>
     <input style="display: none;" type="text" class="section_title_value" value="<?php echo $risk_factor->getQuestion(); ?>" />
 </li>
