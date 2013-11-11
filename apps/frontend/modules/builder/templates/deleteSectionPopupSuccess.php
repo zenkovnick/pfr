44f1742699cde43e3dc83c6a3d6eb44e66a98657
Irina Korolyuk <irina@irina-PC.(none)>
2013-11-11 12:16:43 +0200

    fix for 22512

--- apps/frontend/modules/builder/templates/deleteSectionPopupSuccess.php
+++ apps/frontend/modules/builder/templates/deleteSectionPopupSuccess.php
@@ -1,9 +1,13 @@
-<div id="dialog" title="Confirmation Required">
+<div id="dialog" class="popup-block" title="Confirmation Required">
+    <h1>Delete section</h1>
     <input type="hidden" id="section_id" value="<?php echo $section_id ?>" />
     <input type="hidden" id="obj_ids" value="<?php echo $obj_ids ?>"/>
     <p>Warning!  All the sub-questions will be removed completely as well.</p>
-    <a class='confirm-delete-section'>Confirm</a>
-    <a class='reject-delete-section'>Cancel</a>
+    <div class="button-wrapper">
+        <a class='confirm-delete-section btn btn-red'>Confirm</a>
+        <a class='reject-delete-section btn btn-grey' onclick="jQuery.fancybox.close();">Cancel</a>
+    </div>
+
 </div>
 
 <script type="text/javascript">
