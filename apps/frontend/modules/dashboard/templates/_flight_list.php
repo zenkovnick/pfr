37b6fb1ecddaa3b5484326010cf768b35589fd0b
Irina Korolyuk <irina@irina-PC.(none)>
2013-11-15 10:37:32 +0200

    fix for 23261 & 23527

--- apps/frontend/modules/dashboard/templates/_flight_list.php
+++ apps/frontend/modules/dashboard/templates/_flight_list.php
@@ -12,17 +12,22 @@
                     <li id="<?php echo $flight->getId() ?>">
                 <?php endif ?>
             <?php endif ?>
-            <a href="" class="flight-note-link">
-                <?php echo $flight->getFlightNote() ? 'Update Note' : 'Add Note' ?>
-            </a>
-            <?php if($can_manage): ?>
-                <a href="<?php echo url_for('@delete_risk_assessment_popup?id='.$flight->getId()) ?>" class="delete_risk_assessment fancy">X</a>
-            <?php endif ?>
 
-            <?php if(!is_null($flight->getRiskFactorSum())): ?>
+
+
+                <?php if(!is_null($flight->getRiskFactorSum())): ?>
                     <span class="risk"><?php echo $flight->getRiskFactorSum() ?></span>
                 <?php endif ?>
 
+                <?php if($can_manage): ?>
+                    <a href="<?php echo url_for('@delete_risk_assessment_popup?id='.$flight->getId()) ?>" class="delete_risk_assessment fancy">X</a>
+                <?php endif ?>
+                <?php if($flight->getStatus() == 'complete'): ?>
+                    <a class="send-flight-email-link" href="#">Send</a>
+                <?php endif ?>
+                <a href="" class="flight-note-link">
+                    <?php echo $flight->getFlightNote() ? 'Update Note' : 'Add Note' ?>
+                </a>
                 <a class="name" href="<?php echo url_for(($flight->getDrafted() ? "@edit_flight" : "@view_flight")."?account_id={$account->getId()}&id={$flight->getId()}") ?>">
                     <?php echo $flight->getAirportFrom()->getICAO() ? $flight->getAirportFrom()->getICAO() : "" ?>
                     <?php echo $flight->getAirportTo()->getICAO() ? "- {$flight->getAirportTo()->getICAO()}" : "" ?>
@@ -36,9 +41,7 @@
                     <?php endif ?>
 
                 </a>
-                <?php if($flight->getStatus() == 'complete'): ?>
-                    <a class="send-flight-email-link" href="#">Send</a>
-                <?php endif ?>
+
                 <span class="info">
                     <?php echo "Submitted ".date('m/d/Y Hi', strtotime($flight->getUpdatedAt()))?>
                 </span>
