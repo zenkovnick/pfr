3540e5bca8400d4a8ef2171b96f39cd7151e255e
Mykola Zenkov <mykola.zenkov@masterofcode.com>
2013-11-19 17:54:03 +0200

    Add 0-risk questions

--- lib/model/doctrine/Flight.class.php
+++ lib/model/doctrine/Flight.class.php
@@ -203,14 +203,15 @@ class Flight extends BaseFlight
         foreach($flight_data['risk_analysis'] as $key => $risk_factor)
         {
             $arr = array();
-            if(!is_null($risk_factor['selected_response']) && $risk_factor['response_options'][$risk_factor['selected_response']]['value']>0){
+            //if(!is_null($risk_factor['selected_response']) && $risk_factor['response_options'][$risk_factor['selected_response']]['value']>0){
                 $index = md5($risk_factor['question']);
                 $arr['question'] = $risk_factor['question'];
 //                $arr['answer'] = $risk_factor['response_options'][$risk_factor['selected_response']]['text'];
 //                $arr['risk'] =  $risk_factor['response_options'][$risk_factor['selected_response']]['value'];
 
-                $high_risk_factors[$index] = $risk_factor['question'];
-            }
+                $high_risk_factors[$index]['question'] = $risk_factor['question'];
+                $high_risk_factors[$index]['with_risk'] = (!is_null($risk_factor['selected_response']) && $risk_factor['response_options'][$risk_factor['selected_response']]['value']>0) ? true : false;
+            //}
         }
         return $high_risk_factors;
     }
