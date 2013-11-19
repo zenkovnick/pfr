68f92b4d2a0c0be888bad09975c91214fae6ed9c
Irina Korolyuk <irina@irina-PC.(none)>
2013-11-19 17:40:10 +0200

    fix for lowercase

--- apps/frontend/modules/flight/templates/_flight_note.php
+++ apps/frontend/modules/flight/templates/_flight_note.php
@@ -1,5 +1,5 @@
 <div class='note-form-wrapper'>
-    <textarea class="flight-note" name="flight[flight_note]"><?php echo $flight->getFlightNote() ? $flight->getFlightNote() : '' ?></textarea>
+    <textarea class="flight-note" name="flight[flight_note]" placeholder=""><?php echo $flight->getFlightNote() ? $flight->getFlightNote() : '' ?></textarea>
     <button class="cancel btn btn-grey">Cancel</button>
     <button class="submit btn btn-green">Submit</button>
     <span>Make a note and press Submit</span>
