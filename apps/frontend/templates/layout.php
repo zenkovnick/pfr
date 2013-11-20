cf9d4c617c61a4484aef419ccc3619bb0c439128
Irina Korolyuk <irina@irina-PC.(none)>
2013-11-20 16:59:00 +0200

    placeholder fix

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -27,6 +27,19 @@
                 href: "/css/style_mobile.css"
             }).appendTo("head");
         }
+        jQuery('[placeholder]').focus(function() {
+            var input = jQuery(this);
+            if (input.val() == input.attr('placeholder')) {
+                input.val('');
+                input.removeClass('placeholder');
+            }
+        }).blur(function() {
+                var input = jQuery(this);
+                if (input.val() == '' || input.val() == input.attr('placeholder')) {
+                    input.addClass('placeholder');
+                    input.val(input.attr('placeholder'));
+                }
+            }).blur();
 //        jQuery('input, textarea').placeholder();
     </script>
 </head>
