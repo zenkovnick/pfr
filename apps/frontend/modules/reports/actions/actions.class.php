9c9ed4f540747b315d2ae96af1f51bfe0a816c9c
Mykola Zenkov <mykola.zenkov@masterofcode.com>
2013-11-22 16:36:29 +0200

    wkhtmltopdf

--- apps/frontend/modules/reports/actions/actions.class.php
+++ apps/frontend/modules/reports/actions/actions.class.php
@@ -67,11 +67,18 @@ class reportsActions extends sfActions
           'date_to' => $request->getParameter('date_to')
       ));
 
-      $pdf = new DOMPDF();
-      $pdf->load_html($html);
-      $pdf->render();
-      ob_end_flush();
-      $pdf->stream("hello.pdf");
+//      $pdf = new DOMPDF();
+//      $pdf->set_base_path($request->getUriPrefix()."/css/style.css");
+//      $pdf->load_html($html);
+//      $pdf->render();
+//      ob_end_flush();
+//      $pdf->stream("hello.pdf");
+      $pdf = new WkHtmlToPdf;
+      $pdf->setOptions(array('bin' => '/usr/local/bin'));
+      $pdf->addPage($html);
+
+
+      $pdf->send('test.pdf');
   }
 
 
